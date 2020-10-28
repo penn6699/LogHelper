806a8a09a9dde73098b77a6af9e2b1482a3829d7
penn6699 <penn6699@126.com>
2020-10-29 07:00:06 +0800

    add test

---
+++ api/Controllers/TestController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace api.Controllers
+{
+    public class TestController : ApiController
+    {
+        [HttpGet]
+        public dynamic t1() {
+            LogHelper.Debug("Debug", new { 
+                a=455
+            });
+            LogHelper.Error("Error", new
+            {
+                a = 123
+            });
+            LogHelper.Trace("Error", new
+            {
+                a = 789
+            });
+            return DateTime.Now.Ticks;
+        }
+
+
+
+
+
+    }
+}
